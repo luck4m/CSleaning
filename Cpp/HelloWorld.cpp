0cf70cf17ced3344acf0e67058b66dd6ee224fdd
luck4m <591781585@qq.com>
2021-08-22 18:49:12 +0800

    [Description:]Hello World!

---
+++ Cpp/HelloWorld.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    vector<int> vec{1, 2, 3, 4, 5};
+    for (auto& n : vec) {
+        cout << n << endl;
+    }
+    cout << "Hello World!" << endl;
+    getchar();
+    return 0;
+}
